188a12afd9b3e0d85f27c6815e1644ad8cced9ba
SheilaNS <shei.nsantos@gmail.com>
2022-12-13 20:32:33 -0300

    feat(DB): :card_file_box: add post create validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 // creates the aplication and starts listening on port 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
