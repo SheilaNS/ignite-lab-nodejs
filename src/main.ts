e3fa0ac31667fd000be33efaefc94c6fe9ef4c3c
SheilaNS <shei.nsantos@gmail.com>
2022-12-13 18:55:46 -0300

    chore(Comment): :bulb: Add function bootstrap comment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// creates the aplication and starts listening on port 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
