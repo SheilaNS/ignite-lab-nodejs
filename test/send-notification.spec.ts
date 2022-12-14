1e29bb4f96ecc10103ca5c57079a07ace2febb10
SheilaNS <shei.nsantos@gmail.com>
2022-12-14 17:28:38 -0300

    refactor(UnitTest): reorganizing send-notification unit test

--- test/send-notification.spec.ts
+++ test/send-notification.spec.ts
@@ -1,25 +1,19 @@
-import { Notification } from 'src/app/entities/notification';
 import { SendNotification } from '../src/app/use-cases/send-notification';
-
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
+import { InMemoryNotificationsRepository } from './repositories/in-memory-notifications-repository';
 
 describe('Send notification', () => {
   it('Should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       content: 'This is a notification.',
       category: 'social',
       recipientId: 'example-recipient-id',
     });
 
-    expect(notifications).toHaveLength(1);
-    expect(notifications).toBeTruthy();
+    expect(notificationsRepository.notifications).toBeTruthy();
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
