40444596f1e9127686d01d5089f149f9e4555a39
SheilaNS <shei.nsantos@gmail.com>
2022-12-14 17:17:42 -0300

    test(UnitTest): send-notification unit test update

--- test/send-notification.spec.ts
+++ test/send-notification.spec.ts
@@ -1,15 +1,25 @@
+import { Notification } from 'src/app/entities/notification';
 import { SendNotification } from '../src/app/use-cases/send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('Should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'This is a notification.',
       category: 'social',
       recipientId: 'example-recipient-id',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
+    expect(notifications).toBeTruthy();
   });
 });
