8db3992a032bf9fd97ee2f70eecacf172ab53c6f
SheilaNS <shei.nsantos@gmail.com>
2022-12-14 11:57:34 -0300

    test(UnitTest): add content unit test

---
+++ src/app/entities/content.spec.ts
@@ -0,0 +1,17 @@
+import { Content } from './content';
+
+describe('Create notification content', () => {
+  it('Should be able to create a notification content', () => {
+    const content = new Content('Você recebeu uma solicitação de amizade!');
+
+    expect(content).toBeTruthy();
+  });
+
+  it('Should not be able to create a notification with less than 5 characters', () => {
+    expect(() => new Content('Olá!')).toThrow();
+  });
+
+  it('Should not be able to create a notification with more than 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
+});
