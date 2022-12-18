7f5b61af16d4acc3e91f5820b87ee11c600d0a91
SheilaNS <shei.nsantos@gmail.com>
2022-12-18 17:42:13 -0300

    feat(Code): findById and save methods inplemented

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -7,7 +7,12 @@ export class InMemoryNotificationsRepository
   public notifications: Notification[] = [];
 
   async findById(notificationId: string): Promise<Notification | null> {
-    throw new Error('Method not implemented.');
+    const notification = this.notifications.find(
+      (elem) => elem.id === notificationId,
+    );
+
+    if (!notification) return null;
+    return notification;
   }
 
   async create(notification: Notification) {
@@ -15,6 +20,12 @@ export class InMemoryNotificationsRepository
   }
 
   async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+    const notificationIndex = this.notifications.findIndex(
+      (elem) => elem.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
   }
 }
