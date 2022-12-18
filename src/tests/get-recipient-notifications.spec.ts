e0f7754f0cc11df8265a8138755a3674ed0cc0a5
SheilaNS <shei.nsantos@gmail.com>
2022-12-18 18:53:14 -0300

    test(UnitTest): get-recipient-notifications unit test done

---
+++ src/tests/get-recipient-notifications.spec.ts
@@ -0,0 +1,36 @@
+import { GetRecipientNotifications } from '@app/use-cases/get-recipient-notifications';
+import { makeNotification } from '@test/factories/notification-fatory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+
+describe('Get recipients notifications', () => {
+  it('Should be able to get recipients notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const getRecipientNotifications = new GetRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-2' }),
+    );
+
+    const { notifications } = await getRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+      ]),
+    );
+  });
+});
